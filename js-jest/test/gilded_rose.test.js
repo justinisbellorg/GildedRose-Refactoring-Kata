4e1713e2bd34bd1435c6aad0a0934010af54ae22
Justin Isbell <justinisbell@justinisbell.org>
2020-12-14 10:36:45 -0700

    Basic Test Coverage
    Here I added basic tests for how the code should work. This covers the "moving parts" of the current implementation but it does NOT cover the moving parts of the other implementations where behavior could change. The next step is to add tests to validate those behaviors.

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,42 +1,82 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
-  });
+  describe("Brie",function(){
+    it("should increase in value by 1 when sellby is not past", function() {
+      const gildedRose = new Shop([new Item("Aged Brie", 4, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(11);
+    });
+    //this is how it works now and it seems like it PROBABLY matches what they want.
+    it("should increase in value by 2 once past its sell by date", function() {
+      const gildedRose = new Shop([new Item("Aged Brie", 0, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(12);
+    });
+    it("should never be worth more than 50", function() {
+      const gildedRose = new Shop([new Item("Aged Brie", 4, 50)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(50);
+    });
+  })
+
+  describe("Sulfuras", function(){
+    it("should stay the same when past its sell by date", function() {
+      const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 5, 55)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(55);
+    });
+    it("should stay the same when not past its sell by date", function() {
+      const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 0, 55)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(55);
+    });
+  })
+
+  describe("Backstage Pass", function(){
+    it("should go up in value normally outside of 10 days to sellIn", function(){
+      const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 20, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(11);
+    });
+    it("should go up in value by 2 within 10 days but outside of 5", function(){
+      const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 9, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(12);
+    });
+    it("should go up in value by 3 within 5 days of sell in", function(){
+      const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 4, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(13);
+    })
+    it("Should value 0 once it is passed sell in", function(){
+      const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(0);
+    })
+    it("should never go above 50", function(){
+      const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 4, 50)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(50);
+    })
+  })
+
+  describe("Standard Items",function(){
+    it("should go down in value one day at a time", function(){
+      const gildedRose = new Shop([new Item("Rando", 5, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(9);
+    })
+    it("should never be below 0", function(){
+      const gildedRose = new Shop([new Item("Rando", 4, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(0);
+    })
+    it("Should lose value twice as fast once past sell by", function(){
+      const gildedRose = new Shop([new Item("rando", 0, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(8);
+    })
+  })
 });
 
-// - All items have a SellIn value which denotes the number of days we have to sell the item
-// - All items have a Quality value which denotes how valuable the item is
-// - At the end of each day our system lowers both values for every item
-
-// Pretty simple, right? Well this is where it gets interesting:
-
-// - Once the sell by date has passed, Quality degrades twice as fast
-// - The Quality of an item is never negative
-// - "Aged Brie" actually increases in Quality the older it gets
-// - The Quality of an item is never more than 50
-// - "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
-// - "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
-// Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
-// Quality drops to 0 after the concert
-
-/* Scenario Walkthroughs to help thinking
-***************************
-Here we list out what seem like common scenarios. This plus the logic comments we put in should help us map out how this thing actually works
-1. We don't know if how it works actually matches the rules
-2. We presume anywhere it doesn't work matching the specs is a bug but this is an assumption
-3. Lets assuming this is running live in how we approach making changes and limit the number of breaking changes
-***************************
-
-One big Note is that it looks like we don't have any items changing the value of other items which is great, this basically leaves us with just a few scenarios for each item:
-1. The item is not passed its sell by date
-2. The item is passed its sell by date.
-3. The items value has hit zero
-4. The items value has hit 50
-
-We can probably just makes 4 sets of tests to handle these situations. 
-
-*/
